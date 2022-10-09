0f0e0c0ad7a56c291955320d09bbca7b1efa205a
Ivan Montenegro <nelson.moncar@outlook.com>
2022-10-09 16:03:23 -0500

    Instalar chakra para el uso de css e instalar reacticons

--- src/Launches/index.jsx
+++ src/Launches/index.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import * as API from '../services/launches.js'
 import { useEffect, useState } from 'react';
+import { Box, Flex, Text,Spacer, Tag } from '@chakra-ui/react';
 
 const Launches = () =>{
     const [launches, setLaunches] = useState([]);
 
     useEffect(()=>{
-        API.getAllLaunches() .then(setLaunches);
-    },[]);
+        API.getAllLaunches().then(setLaunches);
+    }, []);
     return (
-        <ul>
+        <section>
             {launches.map(launch=>(
-            <li key={launch.flight_number}>
+            <Box 
+            key={launch.flight_number} 
+            bg='gray.100' 
+            p={4} 
+            m={4} 
+            borderRadius='lg'>
+                <Flex>
+                    <Text fontSize='2xl'>
+                        Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
+                    </Text>
+                    <Spacer/>
+                    <Tag p={4} colorScheme={launch.launch_success ? 'green' : 'red'} >
+                        {launch.launch_success ? 'Success' : 'Failure'}
+                    </Tag>
+                </Flex>
+                <p>
                 {launch.mission_name} ({launch.launch_year})
-            </li>
+                </p>
+            </Box>
             ))}
-        </ul>
+        </section>
     )
 
 }
--- src/Titulo/index.jsx
+++ src/Titulo/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { Heading, Image } from '@chakra-ui/react'
 
 const Title = () => {
     return(
         <>
-        <img src={logo}/>
-        <h1>SpaceX Launches</h1>
+        <Image m={4} src={ logo } alt='Logo-SpaceX' />
+        <Heading as='h1' size='xl' m={4}>SpaceX Launches</Heading>
         </>
     )
 };

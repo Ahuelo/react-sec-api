c9df235d0cfbb14de747088391bf17c722351c46
Ivan Montenegro <nelson.moncar@outlook.com>
2022-10-09 16:37:07 -0500

    anexo react icon, y uso enlazado con chakra

--- src/Launches/index.jsx
+++ src/Launches/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as API from '../services/launches.js'
 import { useEffect, useState } from 'react';
-import { Box, Flex, Text,Spacer, Tag } from '@chakra-ui/react';
+import { LaunchItem } from "../LaunchItem";
 
 const Launches = () =>{
     const [launches, setLaunches] = useState([]);
@@ -12,25 +12,9 @@ const Launches = () =>{
     return (
         <section>
             {launches.map(launch=>(
-            <Box 
-            key={launch.flight_number} 
-            bg='gray.100' 
-            p={4} 
-            m={4} 
-            borderRadius='lg'>
-                <Flex>
-                    <Text fontSize='2xl'>
-                        Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
-                    </Text>
-                    <Spacer/>
-                    <Tag p={4} colorScheme={launch.launch_success ? 'green' : 'red'} >
-                        {launch.launch_success ? 'Success' : 'Failure'}
-                    </Tag>
-                </Flex>
-                <p>
-                {launch.mission_name} ({launch.launch_year})
-                </p>
-            </Box>
+                <LaunchItem 
+                key={launch.flight_number} 
+                {...launch}/>
             ))}
         </section>
     )
